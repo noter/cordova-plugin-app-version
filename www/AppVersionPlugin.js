46417f0aed56dcdf402333814f9d28966b14eecc
noter <noter@eforce.pl>
2013-09-12 16:35:22 +0200

    cordova 3.0 android upgrade

--- www/AppVersionPlugin.js
+++ www/AppVersionPlugin.js
@@ -1,14 +1,23 @@
-// Returns a jQuery deferred object, or pass a success and fail callbacks if you don't want to use jQuery
-var getAppVersion = function (success, fail) {
-  var dfr = null;
-  if ((typeof success) === 'undefined' && window.jQuery) {
-    dfr = jQuery.Deferred();
-    success = dfr.resolve;
-    fail = dfr.reject;
-  }
-  // 5th param is NOT optional. must be at least empty array
-  cordova.exec(success, fail, "AppVersion", "getVersionNumber", []);
-  return dfr;
-};
+cordova.define("cordova/plugin/AppVersion",
 
-module.exports = getAppVersion;
+    function (require, exports, module) {
+
+        var exec = require("cordova/exec");
+
+        /**
+         * Constructor.
+         *
+         * @returns {AppVersion}
+         */
+        function AppVersion() {
+        };
+
+
+        AppVersion.prototype.appVersion = function (successCallback, errorCallback) {
+            exec(successCallback, errorCallback, "AppVersion", "getVersionNumber", []);
+        };
+
+
+        var appVersion = new AppVersion();
+        module.exports = appVersion;
+    });
